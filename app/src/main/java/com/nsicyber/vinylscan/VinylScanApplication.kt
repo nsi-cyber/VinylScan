d1e363486e5ffa7a96433619b7a6caa8a2bec8a1
nsi <jcooligame@gmail.com>
2024-12-25 02:21:22 +0300

    Release 0.1.0

--- app/src/main/java/com/nsicyber/vinylscan/VinylScanApplication.kt
+++ app/src/main/java/com/nsicyber/vinylscan/VinylScanApplication.kt
@@ -10,25 +10,7 @@ import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class VinylScanApplication : Application(){
-    private val apiKey: String
-        get() {
-            val remoteConfig = Firebase.remoteConfig
-            return remoteConfig.getString("api_key")
-        }
-    init {
-        val remoteConfig = Firebase.remoteConfig
-        val configSettings = remoteConfigSettings {
-            minimumFetchIntervalInSeconds = 3600
-        }
-        remoteConfig.setConfigSettingsAsync(configSettings)
-        remoteConfig.setDefaultsAsync(mapOf("api_key" to ""))
-        fetchRemoteConfig()
-    }
-    private fun fetchRemoteConfig() {
-        Firebase.remoteConfig.fetchAndActivate()
-            .addOnCompleteListener { task ->
-            }
-    }
+
     override fun onCreate() {
         super.onCreate()
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
