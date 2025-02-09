3d898964575e08248e94293faa9cf3c38ca92db8
nsi <jcooligame@gmail.com>
2025-02-09 23:24:37 +0300

    Release 1.1.0 (11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.nsicyber.vinylscan"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.0"
+        versionCode = 11
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
